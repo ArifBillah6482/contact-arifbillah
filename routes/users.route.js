eb8a5c020d196027ac7aaceaea775b6a3f8442c8
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-08-28 11:03:17 +0600

    Update users.route.js

--- routes/users.route.js
+++ routes/users.route.js
@@ -8,22 +8,7 @@ const {
 const router = require("express").Router();
 
 router.get("/", getAllUsesr);
-router.post("/", (req, res) => {
-  const newUser = {
-    id: uuidv4(),
-    name: req.body.name,
-    email: req.body.email,
-    msg: req.body.email,
-  };
-  users.push(newUser);
-  res.redirect("/ok");
-});
-router.post("/ok", (req, res) => {
-  res.sendFile(__dirname + "/ok.html");
-});
-router.get("/ok", (req, res) => {
-  res.sendFile(__dirname + "/ok.html");
-});
+router.post("/", createUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
